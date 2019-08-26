b45d23289ae0574466a65485fa595f8b4cdac674
MrExplode <pjanos@outlook.hu>
2019-08-26 19:36:13 +0200

    Bugfixes, improvements
    wired in the advanced stat option (eg. videos, gifs)
    and a lot of bugfix

--- src/main/java/me/mrexplode/messagecounter/Main.java
+++ src/main/java/me/mrexplode/messagecounter/Main.java
@@ -28,42 +28,42 @@ public class Main {
     private static BufferedReader reader;
     
     public static void main(String[] args) throws IOException {
-        System.out.println("---=== Welcome to MrExplolde's Messenger Information tool! ===---\nPlease specify the path your downloaded zip file, or just the single .json file containing the conversation data\nPlease note that if you wish to use this tool, all your downloaded data must be json formatted (as been selected at the Facebook settings)");
+        System.out.println("---=== Welcome to MrExplolde's Messenger Information tool! ===---\nPlease specify the path your downloaded zip file.\n\nPlease note that if you wish to use this tool, all your downloaded data must be json formatted (as been selected at the Facebook settings)");
         reader = new BufferedReader(new InputStreamReader(System.in));
-        File file = new File(reader.readLine());
+        File zipFile = new File(reader.readLine());
         
-        if (!file.exists()) {
+        if (!zipFile.exists()) {
             System.out.println("The specified file doesn't exist!");
             return;
         }
         
-        if (file.isDirectory()) {
-            fromDirectory(file);
-        } else {
-            fromFile(file);
-        }
+        fromDirectory(zipFile);
         
     }
     
-    private static void fromDirectory(File file) {
-        File userdata = new File(workDir, file.getName());
+    private static void fromDirectory(File zipFile) {
+        File userdata = new File(workDir, zipFile.getName());
         userdata.mkdirs();
+        long start = System.currentTimeMillis();
         try {
-            unzip(userdata, file);
+            System.out.println("Uncompressing data...");
+            unzip(userdata, zipFile);
         } catch (IOException e) {
             System.err.println("Failed to uncompress user data archive.\nThings you can try: Place the archive in non-admin right required folder");
             e.printStackTrace();
             return;
         }
+        System.out.println("Done! ("+ (System.currentTimeMillis() - start) + "ms)\n\n");
         
         //checking that the user exported the messaging data from Facebook
-        File[] files = file.listFiles();
+        File[] files = userdata.listFiles();
         File messageFolder = null;
         for (File entry : files)
             if (entry.getName().equals("messages"))
                 messageFolder = entry;
         if (messageFolder == null) {
             System.out.println("You dumb fool! You didn't exported your messaging data! Go back and check that in.");
+            deleteData(userdata);
             return;
         }
         
@@ -77,6 +77,7 @@ public class Main {
         System.out.println("You used " + new File(messageFolder, "stickers_used").listFiles().length + " stickers.");
         System.out.println("\n\nActive conversation details:\n\n");
         File[] inboxList = new File(messageFolder, "inbox").listFiles();
+        //kinda misleading var name, it covers any kind of conversation where the user was involved
         for (File person : inboxList) {
             System.out.println("==============================");
             StringBuilder sb = new StringBuilder();
@@ -104,6 +105,14 @@ public class Main {
             int audioCount = 0;
             int fileCount = 0;
             
+            try {
+                photoCount = new File(person, "photos").listFiles().length;
+                gifCount = new File(person, "gifs").listFiles().length;
+                videoCount = new File(person, "videos").listFiles().length;
+                audioCount = new File(person, "audio").listFiles().length;
+                fileCount = new File(person, "files").listFiles().length;
+            } catch (Exception e) {}
+            
             System.out.println("Conversation title: " + conv.title);
             if (conv.participants.length > 2)
                 System.out.println("Participants in group: " + conv.participants.length);
@@ -127,51 +136,53 @@ public class Main {
         }
         
         //cleanup user sensitive data
-        for (File entry : userdata.listFiles()) {
-            entry.delete();
-        }
-        for (File entry : new File(workDir).listFiles()) {
-            entry.delete();
-        }
-        
+        deleteData(userdata);
         
     }
     
-    private static void fromFile(File file) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String line;
-        StringBuilder builder = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            builder.append(line);
-        }
-        reader.close();
-        
-        Gson gson = new Gson();
-        
-        Conversation conv = gson.fromJson(builder.toString(), Conversation.class);
-        System.out.println("Conversation title: " + conv.title);
-        System.out.println("Messages: " + conv.messages.length);
-        
-        LocalDateTime first = LocalDateTime.ofInstant(Instant.ofEpochMilli(conv.messages[conv.messages.length -1].timestamp_ms), ZoneId.systemDefault());
-        LocalDateTime last = LocalDateTime.ofInstant(Instant.ofEpochMilli(conv.messages[0].timestamp_ms), ZoneId.systemDefault());
-        Duration duration = Duration.between(first, last);
-        
-        System.out.println("Time between first and last message: " + DurationFormatUtils.formatDurationWords(duration.toMillis(), true, true));
+    private static void deleteData(File userdata) {
+        System.out.println("\n\n\n\nDeleting user sensitive data...");
+        deleteFolder(userdata);
+        System.out.println("Done!");
     }
     
+    private static void deleteFolder(File folder) {
+        File[] files = folder.listFiles();
+        if(files!=null) {
+            for(File f: files) {
+                if(f.isDirectory()) {
+                    deleteFolder(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+        folder.delete();
+    }
+    
+    //193693ms: compressed size, /4 buffer
+    //196576ms: compressed size, /2 buffer, logs
+    //198249ms: compressed size, /3 buffer, logs
+    //199837ms: size, /4 buffer, logs
+    //198740ms: compressed size, /4 buffer, w/o logs
     public static void unzip(File dir, File zip) throws IOException {
-        byte[] buffer = new byte[1024 * 2];
         ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zip));
         ZipEntry entry = zipIn.getNextEntry();
+        
         while (entry != null) {
             File newFile = new File(dir, entry.getName());
-            FileOutputStream out = new FileOutputStream(newFile);
-            int len;
-            while ((len = zipIn.read(buffer)) > 0) {
-                out.write(buffer, 0, len);
+            if (!entry.isDirectory()) {
+                //System.out.println("[DEBUG] compSize: " + entry.getCompressedSize() + " size: " + entry.getSize() + " entry: " + entry.getName());
+                byte[] buffer = new byte[(int) (entry.getCompressedSize() < 1024 ? 1024 : entry.getCompressedSize() / 4)];
+                FileOutputStream out = new FileOutputStream(newFile);
+                int len;
+                while ((len = zipIn.read(buffer)) > 0) {
+                    out.write(buffer, 0, len);
+                }
+                out.close();
+            } else {
+                newFile.mkdir();
             }
-            
-            out.close();
             entry = zipIn.getNextEntry();
         }
         
@@ -179,12 +190,22 @@ public class Main {
         zipIn.close();
     }
     
-    public static Conversation unknown(String filename) {
+    private static Conversation unknown(String filename) {
         Conversation conv = new Conversation();
         conv.title = filename;
         conv.messages = new Message[] {};
         conv.is_still_participant = false;
         conv.thread_type = "unknown";
         return conv;
+    }
+    
+    @SuppressWarnings("unused")
+    private static String repair(String string) {
+        string = string.replaceAll("Ã¡", "á")
+                .replaceAll("Ã³", "ó")
+                .replaceAll("Ã", "Á")
+                .replaceAll("Ã©", "é")
+                .replaceAll("", "");
+        return string;
     }
 }
